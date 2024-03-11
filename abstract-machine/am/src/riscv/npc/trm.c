49838e1076ea5a6d1636e574ac2a3600eee5b92f
gftytkklt <602022230063@smail.nju.edu.com>
2024-03-11 23:57:30 +0800

    impl uart config

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -14,13 +14,33 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 #endif
 static const char mainargs[] = MAINARGS;
 
+#define UART_BASE 0x10000000L
+#define UART_THR UART_BASE + 0x00
+#define UART_LDL UART_BASE + 0x00
+#define UART_MDL UART_BASE + 0x01
+#define UART_LCR UART_BASE + 0x03
+#define UART_LSR UART_BASE + 0x05
+
+#define UART_LCR_DLAB 0x80
+#define UART_LSR_THRE 0x20
+
 void putch(char ch) {
-  *(volatile char *)(0x10000000L) = ch;
+  while(!(*(volatile char *)(UART_LSR) & UART_LSR_THRE));
+  *(volatile char *)(UART_THR) = ch;
 }
 
 void halt(int code) {
   asm volatile("mv a0, %0; ebreak" : :"r"(code));
   while (1);
+}
+
+void uart_config_divisor() {
+  char div_enable_mask = UART_LCR_DLAB;
+  *(volatile char *)(UART_LCR) |= div_enable_mask;// enable div access
+  // 100MHz/(16*115200) = 54
+  *(volatile char *)(UART_LDL) = 54;
+  *(volatile char *)(UART_MDL) = 0;
+  *(volatile char *)(UART_LCR) &= ~div_enable_mask;// disable div access
 }
 
 void loader() {
@@ -36,7 +56,10 @@ void loader() {
   }
 }
 
+
+
 void _trm_init() {
+  uart_config_divisor();
   loader();
   int ret = main(mainargs);
   halt(ret);
--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -1,12 +1,12 @@
 AM_SRCS := riscv/npc/start.S \
            riscv/npc/trm.c \
-        #    riscv/npc/ioe.c \
-        #    riscv/npc/timer.c \
-        #    riscv/npc/input.c \
-        #    riscv/npc/cte.c \
-        #    riscv/npc/trap.S \
-        #    platform/dummy/vme.c \
-        #    platform/dummy/mpe.c
+           riscv/npc/ioe.c \
+           riscv/npc/timer.c \
+           riscv/npc/input.c \
+           riscv/npc/cte.c \
+           riscv/npc/trap.S \
+           platform/dummy/vme.c \
+           platform/dummy/mpe.c
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker-soc.ld \
