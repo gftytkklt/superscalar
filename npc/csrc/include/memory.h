7ecf6813c1239d080be64a94afd78a8e86268f4b
gftyt <zzqee@nju.edu.cn>
2026-08-19 12:41:36 +0800

    move dev environment to native ubuntu

--- npc/csrc/include/memory.h
+++ npc/csrc/include/memory.h
@@ -8,4 +8,5 @@ extern "C" void mrom_read(int32_t addr, int32_t *data);
 void init_memory(char *path, int type);
 extern uint8_t *mrom;
 extern uint8_t *flash;
+void load_proc(char *path, void *dest, uint64_t capacity);
 #endif
