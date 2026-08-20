9bc50ea16eaccc087925a9e897e4da6a07a9601b
gftyt <zzqee@nju.edu.cn>
2026-08-20 16:55:01 +0800

    fixedpt api

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -127,35 +127,37 @@ typedef	__uint128_t fixedptud;
 
 /* Multiplies a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_muli(fixedpt A, int B) {
-	return 0;
+	return (fixedpt)((fixedpt)A * (fixedpt)B);
 }
 
 /* Divides a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_divi(fixedpt A, int B) {
-	return 0;
+	return (fixedpt)((fixedpt)A / (fixedpt)B);
 }
 
 /* Multiplies two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
-	return 0;
+	return (((fixedptd)A * (fixedptd)B) >> FIXEDPT_FBITS);
 }
 
 
 /* Divides two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
-	return 0;
+	return (((fixedptd)A / (fixedptd)B) << FIXEDPT_FBITS);
 }
 
 static inline fixedpt fixedpt_abs(fixedpt A) {
-	return 0;
+	return (A > 0) ? A : -A;
 }
 
 static inline fixedpt fixedpt_floor(fixedpt A) {
-	return 0;
+	return (A & ~FIXEDPT_FMASK);
 }
 
 static inline fixedpt fixedpt_ceil(fixedpt A) {
-	return 0;
+	fixedptu frac = (fixedptu)A & FIXEDPT_FMASK;
+	fixedptu nz = (frac | (0 - frac)) >> (FIXEDPT_BITS - 1);
+	return (A & ~FIXEDPT_FMASK) + (fixedpt)(nz << FIXEDPT_FBITS);
 }
 
 /*
